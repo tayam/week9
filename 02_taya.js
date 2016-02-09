a3d6c24bb1455eac01d7df9109b90e085b887c49
taya mcmillan <tayamcmillan@seattleacademy.org>
2016-02-09 14:58:06 -0800

    02_taya.js

--- 02_taya.js
+++ 02_taya.js
@@ -1,4 +1,16 @@
-console.log(process.argv);
-var pin = process.argv[2];
-var status = process.argv[3];
-console.log("pin number " + pin + " turn " + status);
+var pin = process.argv[2]*1;
+console.log(pin);
+var wpi = require('wiring-pi');
+
+//console.log("wpi",wpi);
+wpi.setup('gpio');
+wpi.pinMode(pin*1, wpi.OUTPUT);
+var value = 1;
+function toggleBulb(){
+	wpi.digitalWrite(pin*1,value);
+	console.log("pin",pin, "value",value)
+	value = +!value;
+}
+
+toggleBulb();
+setInterval(toggleBulb,500);
